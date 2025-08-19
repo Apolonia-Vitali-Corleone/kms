139ef48cffa79577b201f9ff496dbe546e27f835
Apolonia-Vitali-Corleone <153711525+Apolonia-Vitali-Corleone@users.noreply.github.com>
2025-08-19 19:59:22 +0800

    feat(frontend): integrate category and knowledge management

--- fend/src/router/index.js
+++ fend/src/router/index.js
@@ -1,17 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import KnowledgeList from '../views/KnowledgeList.vue'
-import KnowledgeForm from '../views/KnowledgeForm.vue'
-import KnowledgeDetail from '../views/KnowledgeDetail.vue'
+import KmsKnowledge from '../views/KmsKnowledge.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/knowledge/list', component: KnowledgeList },
-    { path: '/knowledge/create', component: KnowledgeForm },
-    { path: '/knowledge/edit/:id', component: KnowledgeForm, props: true },
-    { path: '/knowledge/detail/:id', component: KnowledgeDetail, props: true },
-    { path: '*', redirect: '/knowledge/list' }
+    { path: '/', component: KmsKnowledge },
+    { path: '*', redirect: '/' }
   ]
 })
